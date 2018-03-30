49e3cbc24df3d48e3c50caf4671e37fe226a75cd
Cristiana Man <man.cristiana1@gmail.com>
2018-03-30 19:57:05 +0200

    Generated Docs

---
+++ public/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Delish Recipe API",
+  "title": "Delish Recipe API",
+  "url": "https://delish-recipe-api.herokuapp.com",
+  "version": "1.0.0",
+  "description": "API Documentation for Delish Recipe App",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-03-30T17:52:39.952Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
