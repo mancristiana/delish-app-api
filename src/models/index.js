37dbee45225751ce85283f6eaca553ea2a8d38ce
Cristiana Man <man.cristiana1@gmail.com>
2018-06-15 19:52:17 +0200

    #5 Added mongoose dependency

---
+++ src/models/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// See docs: http://mongoosejs.com/docs/promises.html#plugging-in-your-own-promises-library
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI).catch(err => {
+  throwError(`Can't connect to MongoDB`);
+});
+
+let db = mongoose.connection;
+db.once('open', () => {
+  console.log(`Connected to MongoDB`);
+});
+db.on('error', error => {
+  console.log('MongoDB error', error);
+});
+
+module.exports = {
+  db: db
+};
